ceb741d29de038d637968c5c9474ccfdd7696d2c
kimjaebeom1 <gojn0214@gmail.com>
2022-11-28 22:57:04 +0900

    Chrome clone

---
+++ js/quote.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    quote: "We become what we think about.",
+    author: "Earl Nightingale",
+  },
+  {
+    quote:
+      "You will face many defeats in life, but never let yourself be defeated.",
+    author: "Maya Angelou",
+  },
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote:
+      "Many of life’s failures are people who did not realize how close they were to success when they gave up.",
+    author: "Thomas A. Edison",
+  },
+  {
+    quote: "When in doubt, choose change.",
+    author: "Lily leung",
+  },
+  {
+    quote:
+      "Success is not final; failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston S. Churchill",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote: "Opportunities don't happen. You create them.",
+    author: "Chris Grosser",
+  },
+  {
+    quote: "Don't be afraid to give up the good to go for the great.",
+    author: "John D. Rockefeller",
+  },
+  {
+    quote:
+      "Success is walking from failure to failure with no loss of enthusiasm.",
+    author: "Winston Churchill",
+  },
+];
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
