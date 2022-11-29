ffa4af3bc2b73223ec1d5805e4a4a0596671e584
kimjaebeom1 <gojn0214@gmail.com>
2022-11-29 18:05:22 +0900

    momentum css update

--- js/background.js
+++ js/background.js
@@ -1,9 +1,10 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["0.jpeg", "1.jpeg", "3.jpg", "4.jpg", "5.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.className = "bgImage";
 
 document.body.appendChild(bgImage); // body에 html을 추가
